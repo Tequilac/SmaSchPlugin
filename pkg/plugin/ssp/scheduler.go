86a660a7f22618155226fde35bdadfb61258c362
Tequilac <ptekielak@gmail.com>
2023-02-08 21:42:51 +0100

    Add labels

--- pkg/plugin/ssp/scheduler.go
+++ pkg/plugin/ssp/scheduler.go
@@ -32,12 +32,28 @@ func (s *CustomScheduler) Score(ctx context.Context, _ *framework.CycleState, _ 
 	var labels = node.Labels
 	var sum int64 = 0
 	switch labels["sma-mem"] {
-	case "sma-mem-low":
+	case "", "sma-mem-low":
 		sum += 0
 	case "sma-mem-mid":
-		sum += 5
+		sum += 2
 	case "sma-mem-high":
-		sum += 10
+		sum += 4
+	}
+	switch labels["sma-cpu"] {
+	case "", "sma-cpu-low":
+		sum += 0
+	case "sma-cpu-mid":
+		sum += 3
+	case "sma-cpu-high":
+		sum += 5
+	}
+	switch labels["sma-temp"] {
+	case "", "sma-temp-low":
+		sum += 0
+	case "sma-temp-mid":
+		sum += 3
+	case "sma-temp-high":
+		sum += 5
 	}
 	return sum, nil
 }
