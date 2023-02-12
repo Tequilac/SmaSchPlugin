2653d8c627ada513ed3758200fbcb7f0792298e9
Tequilac <58514571+tequilac@users.noreply.github.com>
2023-02-12 12:37:49 +0100

    Adjust build architecture

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 BIN_DIR=output/bin
 
 mkdir -p ${BIN_DIR}
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o=${BIN_DIR}/ssp-scheduler ./cmd/scheduler
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o=${BIN_DIR}/ssp-scheduler ./cmd/scheduler
 docker buildx build --platform linux/arm64 -t tequilac/ssp-scheduler -f docker/Dockerfile --push .
